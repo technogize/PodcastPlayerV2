2ec582ef4e7efc13f8f390048c8ff4306986a98e
Mohammed Rahman <me@morahman.co.uk>
2019-11-24 15:31:37 +0000

    Button test updated to test click event

--- src/Button/index.test.js
+++ src/Button/index.test.js
@@ -6,4 +6,12 @@ describe("Content component tests", () => {
   it("Renders Content", () => {
     shallow(<Button buttonText="warning" buttonClass="is-warning" />);
   });
+
+  it("Test click event", () => {
+    const btnClickCallback = jest.fn();
+
+    const button = shallow(<Button clickHandler={btnClickCallback} />);
+    button.find("button").simulate("click");
+    expect(btnClickCallback.mock.calls.length).toEqual(1);
+  });
 });
