3c67eabbb0edfffe52f5d8e9f34b2243524a6b25
Mohammed Rahman <me@morahman.co.uk>
2020-06-14 16:15:07 +0100

    Add play button and titles to player

--- src/reducers/reducer_now-playing.js
+++ src/reducers/reducer_now-playing.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
-export const nowPlaying = (state = false, action) => {
+export const nowPlaying = (state = {}, action) => {
   if (action.type === 'NOW_PLAYING') {
     return action.payload;
   }
